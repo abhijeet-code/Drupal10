1f2505305b915299fb206f6c9aa1fadab0b29954
Abhi <mymail12@gmail.com>
2026-01-29 23:24:34 +0530

    refactor: Implemented strict - Inject TimeInterface and AccountProxyInterface - Replace \Drupal:: calls with injected services

--- event_registration/src/Form/RegistrationForm.php
+++ event_registration/src/Form/RegistrationForm.php
@@ -4,9 +4,11 @@ namespace Drupal\event_registration\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Database\Connection;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\event_registration\Service\EventRegistrationService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Event Registration Form.
@@ -14,11 +16,11 @@ use Drupal\Core\Mail\MailManagerInterface;
 class RegistrationForm extends FormBase {
 
   /**
-   * The database connection.
+   * The event registration service.
    *
-   * @var \Drupal\Core\Database\Connection
+   * @var \Drupal\event_registration\Service\EventRegistrationService
    */
-  protected $database;
+  protected $eventService;
 
   /**
    * The mail manager.
@@ -28,16 +30,41 @@ class RegistrationForm extends FormBase {
   protected $mailManager;
 
   /**
+   * The time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
    * Constructs a new RegistrationForm.
    *
-   * @param \Drupal\Core\Database\Connection $database
-   *   The database connection.
+   * @param \Drupal\event_registration\Service\EventRegistrationService $event_service
+   *   The event registration service.
    * @param \Drupal\Core\Mail\MailManagerInterface $mail_manager
    *   The mail manager.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current user.
    */
-  public function __construct(Connection $database, MailManagerInterface $mail_manager) {
-    $this->database = $database;
+  public function __construct(
+    EventRegistrationService $event_service,
+    MailManagerInterface $mail_manager,
+    TimeInterface $time,
+    AccountProxyInterface $current_user
+  ) {
+    $this->eventService = $event_service;
     $this->mailManager = $mail_manager;
+    $this->time = $time;
+    $this->currentUser = $current_user;
   }
 
   /**
@@ -45,8 +72,10 @@ class RegistrationForm extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('database'),
-      $container->get('plugin.manager.mail')
+      $container->get('event_registration.service'),
+      $container->get('plugin.manager.mail'),
+      $container->get('datetime.time'),
+      $container->get('current_user')
     );
   }
 
@@ -88,7 +117,7 @@ class RegistrationForm extends FormBase {
       '#required' => TRUE,
     ];
 
-    $categories = $this->getEventCategories();
+    $categories = $this->eventService->getEventCategories();
     $selected_category = $form_state->getValue('event_category');
 
     $form['event_category'] = [
@@ -110,12 +139,7 @@ class RegistrationForm extends FormBase {
 
     $dates = [];
     if ($selected_category) {
-      $dates = $this->getEventDates($selected_category);
-    }
-    
-    // Explicitly set dates empty if no category or dates found
-    if (empty($dates)) {
-        $dates = []; 
+      $dates = $this->eventService->getEventDates($selected_category);
     }
 
     $selected_date = $form_state->getValue('event_date');
@@ -131,7 +155,6 @@ class RegistrationForm extends FormBase {
         'callback' => '::updateEventName',
         'wrapper' => 'event-name-wrapper',
       ],
-      // Hide if no dates available (optional UX improvement, but keeping it simple)
       '#disabled' => empty($dates),
     ];
 
@@ -142,11 +165,7 @@ class RegistrationForm extends FormBase {
 
     $events = [];
     if ($selected_category && $selected_date) {
-      $events = $this->getEventNames($selected_category, $selected_date);
-    }
-    
-     if (empty($events)) {
-        $events = []; 
+      $events = $this->eventService->getEventNames($selected_category, $selected_date);
     }
 
     $form['event_name_wrapper']['event_name'] = [
@@ -156,7 +175,7 @@ class RegistrationForm extends FormBase {
       '#empty_option' => $this->t('- Select Event -'),
       '#required' => TRUE,
       '#validated' => TRUE,
-       '#disabled' => empty($events),
+      '#disabled' => empty($events),
     ];
 
     $form['actions'] = [
@@ -188,44 +207,18 @@ class RegistrationForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    // 1. Validate special characters in text fields
     $text_fields = ['full_name', 'college_name', 'department'];
     foreach ($text_fields as $field) {
       $value = $form_state->getValue($field);
       if (!preg_match('/^[a-zA-Z0-9\s]+$/', $value)) {
-        $form_state->setErrorByName($field, $this->t('@field contains speial characters which are not allowed.', ['@field' => $form[$field]['#title']]));
+        $form_state->setErrorByName($field, $this->t('@field contains special characters which are not allowed.', ['@field' => $form[$field]['#title']]));
       }
     }
 
-    // 2. Validate Email Format (Drupal's #type => email does this, but custom check can be added)
-    // Implicitly handled by #type email
-
-    // 3. Prevent duplicate registrations (Email + Event Date)
     $email = $form_state->getValue('email');
-    $event_id = $form_state->getValue('event_name'); // This gets the event ID as key
-
-    // Need to fetch event date to check duplicates by Email + Date
-    // Actually requirement says "Prevent duplicate registrations using: Email + Event Date"
-    // So if I register for Event A on Jan 1, I cannot register for Event B on Jan 1?
-    // Or is it Email + specific Event?
-    // "Event Name (dropdown menu)... lists only the events... using Ajax" implies we are picking an ID.
-    // Let's assume Email + Event Date is the strict rule.
     $event_date = $form_state->getValue('event_date');
 
-    // We need to check if this email has already registered for ANY event on this date.
-    // Query event_registrations joined with event_config
-    
-    // NOTE: The 'event_date' value in form_state is the date string because that's what we put in options?
-    // Wait, getEventDates returns [date => date]. So value is date.
-    
-    $query = $this->database->select('event_registration_data', 'd');
-    $query->join('event_registration_config', 'c', 'd.event_id = c.id');
-    $query->fields('d', ['id']);
-    $query->condition('d.email', $email);
-    $query->condition('c.event_date', $event_date);
-    $result = $query->execute()->fetchField();
-
-    if ($result) {
+    if ($this->eventService->isDuplicateRegistration($email, $event_date)) {
       $form_state->setErrorByName('email', $this->t('You have already registered for an event on @date.', ['@date' => $event_date]));
     }
   }
@@ -236,36 +229,32 @@ class RegistrationForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $values = $form_state->getValues();
     $event_id = $values['event_name'];
-    
-    // Get event details for email
+
     $event_name = $form['event_name_wrapper']['event_name']['#options'][$event_id];
-    
-    try {
-      $this->database->insert('event_registration_data')
-        ->fields([
-          'event_id' => $event_id,
-          'full_name' => $values['full_name'],
-          'email' => $values['email'],
-          'college_name' => $values['college_name'],
-          'department' => $values['department'],
-          'event_category' => $values['event_category'],
-          'created' => \Drupal::time()->getRequestTime(),
-        ])
-        ->execute();
 
+    $data = [
+      'event_id' => $event_id,
+      'full_name' => $values['full_name'],
+      'email' => $values['email'],
+      'college_name' => $values['college_name'],
+      'department' => $values['department'],
+      'event_category' => $values['event_category'],
+      'created' => $this->time->getRequestTime(),
+    ];
+
+    $result = $this->eventService->saveRegistration($data);
+
+    if ($result) {
       $this->messenger()->addStatus($this->t('Registration successful!'));
-      
-      // Send Emails
       $this->sendRegistrationEmails($values, $event_name);
-
     }
-    catch (\Exception $e) {
+    else {
       $this->messenger()->addError($this->t('Registration failed. Please try again.'));
     }
   }
 
   /**
-   * Helper to send emails.
+   * Sends registration emails.
    */
   protected function sendRegistrationEmails($values, $event_name) {
     $module = 'event_registration';
@@ -273,78 +262,29 @@ class RegistrationForm extends FormBase {
     $to = $values['email'];
     $params['event_name'] = $event_name;
     $params['message'] = $this->t("Hello @name,\n\nYou have successfully registered for @event on @date.\n\nCategory: @cat", [
-        '@name' => $values['full_name'],
-        '@event' => $event_name,
-        '@date' => $values['event_date'],
-        '@cat' => $values['event_category'],
+      '@name' => $values['full_name'],
+      '@event' => $event_name,
+      '@date' => $values['event_date'],
+      '@cat' => $values['event_category'],
     ]);
-    $langcode = \Drupal::currentUser()->getPreferredLangcode();
+    $langcode = $this->currentUser->getPreferredLangcode();
     $send = TRUE;
 
-    // Send to User
-    $result = $this->mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
-    
-    // Send to Admin if enabled
-    $config = \Drupal::config('event_registration.settings');
+    $this->mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+
+    $config = $this->eventService->getSettings();
     if ($config->get('enable_admin_notifications')) {
-        $admin_email = $config->get('admin_email');
-        if ($admin_email) {
-            $key_admin = 'admin_notification';
-            $params['message'] = $this->t("New Registration:\nName: @name\nEvent: @event\nDate: @date", [
-                '@name' => $values['full_name'],
-                '@event' => $event_name,
-                '@date' => $values['event_date']
-            ]);
-            $this->mailManager->mail($module, $key_admin, $admin_email, $langcode, $params, NULL, $send);
-        }
+      $admin_email = $config->get('admin_email');
+      if ($admin_email) {
+        $key_admin = 'admin_notification';
+        $params['message'] = $this->t("New Registration:\nName: @name\nEvent: @event\nDate: @date", [
+          '@name' => $values['full_name'],
+          '@event' => $event_name,
+          '@date' => $values['event_date'],
+        ]);
+        $this->mailManager->mail($module, $key_admin, $admin_email, $langcode, $params, NULL, $send);
+      }
     }
-  }
-
-  /**
-   * Helper to get categories.
-   */
-  protected function getEventCategories() {
-    // We fetch categories that have active configurations within the date range?
-    // Requirement says: "Event Registration Form (this form should be available between the start and end date mentioned in the event config page)"
-    // So distinct categories from valid events.
-    $today = date('Y-m-d');
-    $query = $this->database->select('event_registration_config', 'c');
-    $query->fields('c', ['event_category']);
-    $query->condition('registration_start_date', $today, '<=');
-    $query->condition('registration_end_date', $today, '>=');
-    $query->distinct();
-    $result = $query->execute()->fetchAllKeyed(0, 0);
-    return $result;
-  }
-
-  /**
-   * Helper to get dates for a category.
-   */
-  protected function getEventDates($category) {
-    $today = date('Y-m-d');
-    $query = $this->database->select('event_registration_config', 'c');
-    $query->fields('c', ['event_date']);
-    $query->condition('event_category', $category);
-    $query->condition('registration_start_date', $today, '<=');
-    $query->condition('registration_end_date', $today, '>=');
-    $query->distinct();
-    $result = $query->execute()->fetchAllKeyed(0, 0);
-    return $result;
-  }
-
-  /**
-   * Helper to get events for a category and date.
-   */
-  protected function getEventNames($category, $date) {
-    $today = date('Y-m-d');
-    $query = $this->database->select('event_registration_config', 'c');
-    $query->fields('c', ['id', 'event_name']);
-    $query->condition('event_category', $category);
-    $query->condition('event_date', $date);
-     $query->condition('registration_start_date', $today, '<=');
-    $query->condition('registration_end_date', $today, '>=');
-    $result = $query->execute()->fetchAllKeyed();
-    return $result;
   }
 
 }
